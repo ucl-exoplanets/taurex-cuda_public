7a7e37d9d3b93054b51b23108143f20ff3c3b59a
Ahmed <ahmed.f.alrefaie@gmail.com>
2019-12-26 23:45:05 +0000

    Rayleigh scattering completed

---
+++ taurex_cuda/contributions/rayleigh.py
@@ -0,0 +1,114 @@
+from .cudacontribution import CudaContribution
+import numpy as np
+from ..opacity.cudacache import CudaOpacityCache
+from pycuda.gpuarray import GPUArray, to_gpu
+from pycuda.tools import DeviceMemoryPool
+from pycuda.compiler import SourceModule
+from functools import lru_cache
+import math
+
+
+
+def __rayleigh_kernal(molecule_computation_code):
+
+    code = f"""
+
+    __global__ void compute_rayleigh_sigma(double* __restrict__ dest, const double* __restrict__ wngrid,
+                                       const double* __restrict__ mix, const int nlayers, const int grid_size)
+    {{
+
+
+        unsigned int i = (blockIdx.x * blockDim.x) + threadIdx.x;
+        unsigned int j = (blockIdx.y * blockDim.y) + threadIdx.y;
+
+        if (i > grid_size)
+            return;
+        
+        if (j > nlayers)
+            return;
+
+        double _wn = wngrid[i];
+        double _mix = mix[j];
+
+        {molecule_computation_code}
+
+        dest[j*grid_size + i] = result*_mix;
+    }}
+
+    """
+    return code
+@lru_cache(maxsize=-1)
+def get_rayleigh_kernal(molecule):
+    from ..utils.scattering import _molecule_func_code
+    try:
+        molecule_code = _molecule_func_code[molecule]
+    except KeyError:
+        return None
+
+    kernal_code = __rayleigh_kernal(molecule_code)
+    mod = SourceModule(kernal_code)
+
+    return mod.get_function('compute_rayleigh_sigma')
+
+
+
+class RayleighCuda(CudaContribution):
+    """
+    Computes the contribution to the optical depth
+    occuring from molecular Rayleigh.
+    """
+
+    def __init__(self):
+        super().__init__('Rayleigh')
+        self._memory_pool = DeviceMemoryPool()
+
+    def build(self, model):
+        super().build(model)
+        self._mix_array = GPUArray(shape=(model.nLayers,), dtype=np.float)
+
+    def prepare_each(self, model, wngrid):
+        """
+        Prepares each molecular opacity by weighting them
+        by their mixing ratio in the atmosphere
+
+        Parameters
+        ----------
+        model: :class:`~taurex.model.model.ForwardModel`
+            Forward model
+
+        wngrid: :obj:`array`
+            Wavenumber grid
+
+        Yields
+        ------
+        component: :obj:`tuple` of type (str, :obj:`array`)
+            Name of molecule and weighted opacity
+
+        """
+        from taurex.util.scattering import rayleigh_sigma_from_name
+
+        self.debug('Preparing model with %s', wngrid.shape)
+        self._ngrid = wngrid.shape[0]
+        gpu_wngrid = to_gpu(wngrid,allocator=self._memory_pool.allocate)
+        sigma_xsec = GPUArray(shape=(model.nLayers, wngrid.shape[0]), dtype=np.float64, allocator=self._memory_pool.allocate)
+
+        THREAD_PER_BLOCK_X = 16
+        THREAD_PER_BLOCK_Y = 16
+        
+        NUM_BLOCK_X = int(math.ceil(self._ngrid/THREAD_PER_BLOCK_X))
+        NUM_BLOCK_Y = int(math.ceil(model.nLayers/THREAD_PER_BLOCK_Y))
+
+        molecules = model.chemistry.activeGases + model.chemistry.inactiveGases
+
+        for gasname in molecules:
+            sigma = get_rayleigh_kernal(gasname)
+
+            if sigma is not None:
+
+                self._mix_array.set(model.chemistry.get_gas_mix_profile(gasname))
+
+                sigma(sigma_xsec, gpu_wngrid, self._mix_array, np.int32(model.nLayers),np.int32(self._ngrid),
+                      block=(THREAD_PER_BLOCK_X, THREAD_PER_BLOCK_Y,1), grid=(NUM_BLOCK_X, NUM_BLOCK_Y,1) )
+
+                self.sigma_xsec = sigma_xsec
+                yield gasname, sigma_xsec
---
+++ taurex_cuda/utils/scattering.py
@@ -0,0 +1,70 @@
+
+
+
+def compute_h2():
+    
+    code = """
+
+    double wave = 1e8/_wn;
+
+    double wave_2 = 1.0/(wave*wave);
+    double wave_4 = wave_2*wave_2;
+
+    double result = ((8.14E-13)*(wave_4)*(1+(1.572E6)*(wave_2)+(1.981E12)*(wave_4)))*1E-4;
+    """
+
+    return code
+def compute_he():
+    code = """
+
+    double wave = 1e8/_wn;
+
+    double wave_2 = 1.0/(wave*wave);
+    double wave_4 = wave_2*wave_2;
+
+    double result = ((5.484E-14)*(wave_4)*(1+(2.44E5)*(wave_2)))*1E-4;
+    """
+
+    return code
+
+
+def compute_ray_sigma(n=0.0,n_air = 2.6867805e25,king=1.0,):
+    from taurex.constants import PI
+    code = f"""
+    double _wl = (10000.0/_wn)*1e-6;
+    double _wl_2 = 1/(_wl*_wl);
+    double _n = {n};
+    double _nair = {n_air};
+    double _king = {king};
+    double n_factor = (_n*_n - 1)/(_nair*(_n*_n + 2.0));
+
+
+    double result = 24.0*({PI**3.0})*_king*(n_factor*n_factor)/(_wl*_wl*_wl*_wl);
+    """
+
+    return code
+
+_molecule_func_code = {
+        'He' : compute_he(),
+        'H2' : compute_h2(),
+        'N2' : compute_ray_sigma(
+                            n='1. + (6498.2 + (307.43305e12)/(14.4e9 - _wn*_wn))*1.0e-8',
+                            king='1.034+3.17e-12*_wn*_wn'),
+        'O2' : compute_ray_sigma(
+                            n='1 + 1.181494e-4 + 9.708931e-3/(75.4-_wl_2)',
+                            king = 1.096),
+        'CO2' : compute_ray_sigma(
+                        n='1 + 6.991e-2/(166.175-_wl_2)+1.44720e-3/(79.609-_wl_2)+6.42941e-5/(56.3064-_wl_2)'
+                                +'+5.21306e-5/(46.0196-_wl_2)+1.46847e-6/(0.0584738-_wl_2)',king = 1.1364), 
+        'CH4' : compute_ray_sigma(
+                                n='1 + 1.e-8*(46662. + 4.02*1.e-6*pow(1/((10000/_wn)*1.e-4),2.0))'),
+        'CO' : compute_ray_sigma(
+                                n='1.0 + 32.7e-5 * (1.0 + 8.1e-3 / _wl_p2)',king=1.016),
+        'NH3' : compute_ray_sigma(
+                                n='1 + 37.0e-5 * (1. + 12.0e-3 / _wl_p2)'),
+        'H2O' : (lambda ns_air='(1 + (0.05792105/(238.0185 - (10000/(_wn*_wn))) + 0.00167917/(57.362-(10000/(_wn*_wn)))))',delta=0.17:
+                        compute_ray_sigma(n=f'0.85 * ({ns_air} - 1.) + 1',king=f'(6.+3.*{delta})/(6.-7.*{delta})'))(),
+            
+
+
+}
