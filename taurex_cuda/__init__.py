183170a88fc37773fea9ce1a0f68a2c84b4e0280
Ahmed <ahmed.f.alrefaie@gmail.com>
2019-12-28 15:07:42 +0000

    exposed emission model

--- taurex_cuda/__init__.py
+++ taurex_cuda/__init__.py
@@ -1,4 +1,5 @@
 from .model.transmission import TransmissionCudaModel
+from .model.emission import EmissionCudaModel
 from .contributions.absorption import AbsorptionCuda
 from .contributions.cia import CIACuda
 from .contributions.rayleigh import RayleighCuda
