224dd3936603b1d9766057834eb1162eb4050c7c
Ahmed <ahmed.f.alrefaie@gmail.com>
2020-02-02 03:33:13 +0000

    Made contributions discoverable

--- taurex_cuda/__init__.py
+++ taurex_cuda/__init__.py
@@ -3,3 +3,5 @@ from .model.emission import EmissionCudaModel
 from .contributions.absorption import AbsorptionCuda
 from .contributions.cia import CIACuda
 from .contributions.rayleigh import RayleighCuda
+from .contributions.flatmie import FlatMieCuda
+from .contributions.simpleclouds import SimpleCloudsCuda
